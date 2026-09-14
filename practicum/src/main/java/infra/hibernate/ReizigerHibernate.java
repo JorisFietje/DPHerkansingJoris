0a5bb05f9250f164681914e8e4a05931b8d96e0c
jorisfietje <joris.fietje@gmail.com>
2026-09-14 23:17:08 +0200

    P2h: ReizigerHibernate CRUD + persistence.xml geconfigureerd

--- practicum/src/main/java/infra/hibernate/ReizigerHibernate.java
+++ practicum/src/main/java/infra/hibernate/ReizigerHibernate.java
@@ -5,41 +5,48 @@ import domain.Reiziger;
 import jakarta.persistence.EntityManager;
 
 import java.sql.Date;
-import java.sql.SQLException;
 import java.util.List;
 
 public class ReizigerHibernate implements IReizigerDao {
 
-    private EntityManager entityManager;
+    private final EntityManager entityManager;
 
     public ReizigerHibernate(EntityManager entityManager) {
-
+        this.entityManager = entityManager;
     }
 
     @Override
-    public void save(Reiziger reiziger) throws SQLException {
+    public void save(Reiziger reiziger) {
+        entityManager.persist(reiziger);
     }
 
     @Override
-    public void update(Reiziger reiziger) throws SQLException {
+    public void update(Reiziger reiziger) {
+        entityManager.merge(reiziger);
     }
 
     @Override
-    public void delete(Reiziger reiziger) throws SQLException {
+    public void delete(Reiziger reiziger) {
+        entityManager.remove(reiziger);
     }
 
     @Override
-    public Reiziger findById(int id) throws SQLException {
-        return null;
+    public Reiziger findById(int id) {
+        return entityManager.find(Reiziger.class, id);
     }
 
     @Override
     public List<Reiziger> findByGeboorteDatum(Date date) {
-        return null;
+        return entityManager
+                .createQuery("SELECT r FROM Reiziger r WHERE r.geboortedatum = :datum", Reiziger.class)
+                .setParameter("datum", date)
+                .getResultList();
     }
 
     @Override
-    public List<Reiziger> findAll() throws SQLException {
-        return null;
+    public List<Reiziger> findAll() {
+        return entityManager
+                .createQuery("SELECT r FROM Reiziger r", Reiziger.class)
+                .getResultList();
     }
 }
