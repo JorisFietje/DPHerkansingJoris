eec629cf60082eee6083663d183105edeb97f1b1
jorisfietje <joris.fietje@gmail.com>
2026-09-14 22:16:55 +0200

    pgadmin dingen aangepast waardoor de test nu allemaal slagen. screenshot output toegevoegd

--- opdracht/S2_DML_student.sql
+++ opdracht/S2_DML_student.sql
@@ -173,6 +173,7 @@ ORDER BY resultaat;
 
 
 -- Draai alle wijzigingen terug om conflicten in komende opdrachten te voorkomen.
+SET DATESTYLE = 'DMY';
 UPDATE medewerkers SET afd = NULL WHERE mnr < 7369 OR mnr > 7934;
 UPDATE afdelingen SET hoofd = NULL WHERE anr > 40;
 DELETE FROM afdelingen WHERE anr > 40;
