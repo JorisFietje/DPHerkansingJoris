6b206493b96fe3442ac13c4cb2b6e85e9c218764
jorisfietje <joris.fietje@gmail.com>
2026-09-14 21:55:05 +0200

    2.2,3,4 gemaakt

--- opdracht/S2_DML_student.sql
+++ opdracht/S2_DML_student.sql
@@ -38,20 +38,25 @@ SELECT code, omschrijving FROM cursussen WHERE lengte = 4;
 --
 -- Geef alle informatie van alle medewerkers, gesorteerd op functie,
 -- en per functie op leeftijd (van jong naar oud).
--- DROP VIEW IF EXISTS s2_2; CREATE OR REPLACE VIEW s2_2 AS                                                     -- [TEST]
+
+DROP VIEW IF EXISTS s2_2; CREATE OR REPLACE VIEW s2_2 AS
+SELECT * FROM medewerkers ORDER BY functie, gbdatum DESC;                                                    -- [TEST]
 
 
 -- S2.3. Door het land
 --
 -- Welke cursussen zijn in Utrecht en/of in Maastricht uitgevoerd? Geef
 -- code en begindatum.
--- DROP VIEW IF EXISTS s2_3; CREATE OR REPLACE VIEW s2_3 AS                                                     -- [TEST]
+
+DROP VIEW IF EXISTS s2_3; CREATE OR REPLACE VIEW s2_3 AS                                                     -- [TEST]
+SELECT cursus, begindatum FROM uitvoeringen WHERE locatie IN ('UTRECHT', 'MAASTRICHT');
 
 
 -- S2.4. Namen
 --
 -- Geef de naam en voorletters van alle medewerkers, behalve van R. Jansen.
--- DROP VIEW IF EXISTS s2_4; CREATE OR REPLACE VIEW s2_4 AS                                                     -- [TEST]
+DROP VIEW IF EXISTS s2_4; CREATE OR REPLACE VIEW s2_4 AS                                                     -- [TEST]
+SELECT naam, voorl FROM medewerkers WHERE NOT (naam = 'JANSEN' AND voorl = 'R');
 
 
 -- S2.5. Nieuwe SQL-cursus
