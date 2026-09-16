dadd665070b343b77ec29b4d6fcc1d91677ac640
jorisfietje <joris.fietje@gmail.com>
2026-09-17 00:07:27 +0200

    P3: Adres domeinklasse uitgewerkt

--- practicum/src/main/java/domain/Adres.java
+++ practicum/src/main/java/domain/Adres.java
@@ -2,52 +2,75 @@ package domain;
 
 public class Adres {
 
+    private int adresId;
+    private String postcode;
+    private String huisnummer;
+    private String straat;
+    private String woonplaats;
     private Reiziger reiziger;
 
+    public Adres() {
+    }
+
+    public Adres(int adresId, String postcode, String huisnummer, String straat, String woonplaats) {
+        this.adresId = adresId;
+        this.postcode = postcode;
+        this.huisnummer = huisnummer;
+        this.straat = straat;
+        this.woonplaats = woonplaats;
+    }
+
     public int getAdresId() {
-        return 0;
+        return adresId;
     }
 
     public void setAdresId(int adresId) {
-
+        this.adresId = adresId;
     }
 
     public String getPostcode() {
-        return null;
+        return postcode;
     }
 
     public void setPostcode(String postcode) {
-
+        this.postcode = postcode;
     }
 
     public String getHuisnummer() {
-        return null;
+        return huisnummer;
     }
 
     public void setHuisnummer(String huisnummer) {
-
+        this.huisnummer = huisnummer;
     }
 
     public String getStraat() {
-        return null;
+        return straat;
     }
 
     public void setStraat(String straat) {
-
+        this.straat = straat;
     }
 
     public String getWoonplaats() {
-        return null;
+        return woonplaats;
     }
 
     public void setWoonplaats(String woonplaats) {
-
+        this.woonplaats = woonplaats;
     }
 
     public Reiziger getReiziger() {
-        return null;
+        return reiziger;
     }
 
     public void setReiziger(Reiziger reiziger) {
+        this.reiziger = reiziger;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("Adres [id=%d, %s %s, %s %s]",
+                adresId, straat, huisnummer, postcode, woonplaats);
     }
 }
