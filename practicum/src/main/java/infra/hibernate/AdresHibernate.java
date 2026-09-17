2019a8df86354ff48b6eac5083e6366210651fa7
jorisfietje <joris.fietje@gmail.com>
2026-09-17 11:24:55 +0200

    P3H: AdresHibernate uitgewerkt. Alle tests slagen.

--- practicum/src/main/java/infra/hibernate/AdresHibernate.java
+++ practicum/src/main/java/infra/hibernate/AdresHibernate.java
@@ -4,39 +4,50 @@ import domain.Adres;
 import domain.Reiziger;
 import jakarta.persistence.EntityManager;
 
-import java.sql.SQLException;
 import java.util.List;
 
 public class AdresHibernate implements domain.IAdresDao {
 
+    private final EntityManager entityManager;
+
     public AdresHibernate(EntityManager entityManager) {
-
+        this.entityManager = entityManager;
     }
 
     @Override
-    public void save(Adres adres) throws SQLException {
+    public void save(Adres adres) {
+        entityManager.persist(adres);
     }
 
     @Override
-    public void update(Adres adres) throws SQLException {
+    public void update(Adres adres) {
+        entityManager.merge(adres);
     }
 
     @Override
-    public void delete(Adres adres) throws SQLException {
+    public void delete(Adres adres) {
+        entityManager.remove(adres);
     }
 
     @Override
-    public Adres findById(int id) throws SQLException {
-        return null;
+    public Adres findById(int id) {
+        return entityManager.find(Adres.class, id);
     }
 
     @Override
-    public Adres findByReiziger(Reiziger reiziger) throws SQLException {
-        return null;
+    public Adres findByReiziger(Reiziger reiziger) {
+        List<Adres> adressen = entityManager
+                .createQuery("SELECT a FROM Adres a WHERE a.reiziger = :reiziger", Adres.class)
+                .setParameter("reiziger", reiziger)
+                .getResultList();
+
+        return adressen.isEmpty() ? null : adressen.get(0);
     }
 
     @Override
-    public List<Adres> findAll() throws SQLException {
-        return null;
+    public List<Adres> findAll() {
+        return entityManager
+                .createQuery("SELECT a FROM Adres a", Adres.class)
+                .getResultList();
     }
 }
